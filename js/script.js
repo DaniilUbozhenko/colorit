961017fa09fbbb21c92cdd8fa21432b6db34ec5c
DaniilUbozhenko <67505296+DaniilUbozhenko@users.noreply.github.com>
2021-08-04 15:56:04 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -95,3 +95,16 @@ var mySwiper = new Swiper('.popular-slider__container', {
       prevEl: ".header-popular__prev",
    },
 });
+var mySwiper = new Swiper('.reviews-slider__container', {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   loop: true,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+});
