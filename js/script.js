3b2d5872d1a08bf1774bb3319f98cef831afc5fa
DaniilUbozhenko <67505296+DaniilUbozhenko@users.noreply.github.com>
2021-10-11 10:18:55 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,25 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-	if (window.innerWidth <= 767.98) {
-
-	}
-	else if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky");
-	}
-	else {
-		navbar.classList.remove("sticky");
-	}
-}
-
-
-
 // Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.top-menu__body',);
 if (iconMenu) {
-	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
+// Sticky Меню
+window.onscroll = function () { myFunction() };
+var navbar = document.getElementById("bottom-header");
+var sticky = navbar.offsetTop;
+function myFunction() {
+   if (window.innerWidth <= 767.98) {
+
+   }
+   else if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+   }
+   else {
+      navbar.classList.remove("sticky");
+   }
 }
